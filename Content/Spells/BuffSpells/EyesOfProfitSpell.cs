06b3864305a5dc5e4ac0f60095449b5a83880bb7
Aurocosh <Aurocosh@gmail.com>
2022-02-24 21:08:29 +0700

    Added eyes of profit spell

---
+++ Content/Spells/BuffSpells/EyesOfProfitSpell.cs
@@ -0,0 +1,17 @@
+﻿using Spellwright.Content.Spells.Base.Types;
+using Spellwright.Util;
+using Terraria.ID;
+
+namespace Spellwright.Content.Spells.BuffSpells
+{
+    internal class EyesOfProfitSpell : BuffSpell
+    {
+        public override void SetStaticDefaults()
+        {
+            reagentType = ItemID.GoldCoin;
+            reagentUseCost = 2;
+
+            AddEffect(BuffID.Spelunker, (playerLevel) => UtilTime.MinutesToTicks(10 + 2 * playerLevel));
+        }
+    }
+}
