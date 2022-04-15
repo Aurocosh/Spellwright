878a5785bf8a42bbea2d833668125457a0b574db
Aurocosh <Aurocosh@gmail.com>
2022-04-15 17:06:57 +0700

    Added call of the depths spell

---
+++ Content/Buffs/Spells/CallOfTheDepthsBuff.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Spellwright.Content.Buffs.Spells
+{
+    public class CallOfTheDepthsBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Call of the Depths");
+            Description.SetDefault("Core of the world calls for you.");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            float surfaceY = (float)(Main.worldSurface * 16f);
+            if (player.position.Y > surfaceY)
+            {
+                float depth = player.position.Y - surfaceY;
+                float maxDepth = Main.bottomWorld - surfaceY;
+
+                float proportion = depth / maxDepth;
+                if (proportion < .2f)
+                    proportion = .2f;
+                else if (proportion > .9f)
+                    proportion = 1f;
+                float maxBonus = .5f;
+                player.pickSpeed -= maxBonus * proportion;
+            }
+        }
+    }
+}
---
+++ Content/Spells/BuffSpells/CallOfTheDepthsSpell.cs
@@ -0,0 +1,20 @@
+﻿using Spellwright.Content.Buffs.Spells;
+using Spellwright.Content.Spells.Base;
+using Spellwright.Content.Spells.Base.Types;
+using Spellwright.Util;
+using Terraria.ModLoader;
+
+namespace Spellwright.Content.Spells.BuffSpells
+{
+    internal class CallOfTheDepthsSpell : BuffSpell
+    {
+        public override void SetStaticDefaults()
+        {
+            SpellLevel = 5;
+            AddEffect(ModContent.BuffType<CallOfTheDepthsBuff>(), (playerLevel) => UtilTime.MinutesToTicks((int)(2f * playerLevel)));
+
+            AddApplicableModifier(SpellModifier.IsDispel);
+            AddApplicableModifier(SpellModifier.IsEternal);
+        }
+    }
+}
