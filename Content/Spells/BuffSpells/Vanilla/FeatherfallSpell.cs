bff25b76f494f341119389800cb0f34326ac415d
Aurocosh <Aurocosh@gmail.com>
2022-04-18 21:37:27 +0700

    Added premonition and featherfall spells

---
+++ Content/Spells/BuffSpells/Vanilla/FeatherfallSpell.cs
@@ -0,0 +1,19 @@
+﻿using Spellwright.Content.Spells.Base;
+using Spellwright.Content.Spells.Base.Types;
+using Spellwright.Util;
+using Terraria.ID;
+
+namespace Spellwright.Content.Spells.BuffSpells
+{
+    internal class FeatherfallSpell : BuffSpell
+    {
+        public override void SetStaticDefaults()
+        {
+            SpellLevel = 0;
+            AddEffect(BuffID.Featherfall, (playerLevel) => UtilTime.MinutesToTicks(10 + 2 * playerLevel));
+
+            AddApplicableModifier(SpellModifier.IsDispel);
+            AddApplicableModifier(SpellModifier.IsEternal);
+        }
+    }
+}
