601c5b9a7f65e4946ae0896c637fa6cb2421904e
Aurocosh <Aurocosh@gmail.com>
2022-08-26 20:29:20 +0700

    Added mod item spawn to chests

---
+++ Common/System/TomeSpawnSystem.cs
@@ -0,0 +1,63 @@
+﻿using Spellwright.Content.Items;
+using Spellwright.Content.Items.SpellTomes;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Spellwright.Common.System
+{
+    internal class ChestItemSpawnSystem : ModSystem
+    {
+        // We can use PostWorldGen for world generation tasks that don't need to happen between vanilla world generation steps.
+        public override void PostWorldGen()
+        {
+            PlaceItemsInChests(ModContent.ItemType<SpellResonator>(), 3, new int[] { 0, 1 });
+            PlaceItemsInChests(ModContent.ItemType<BeginnerSpellTome>(), 40, new int[] { 0, 1 });
+            PlaceItemsInChests(ModContent.ItemType<AdvancedSpellTome>(), 30, new int[] { 0, 1 });
+            PlaceItemsInChests(ModContent.ItemType<SupremeSpellTome>(), 20, new int[] { 0, 1, 2 });
+        }
+
+        private static void PlaceItemsInChests(int itemTypeId, int count, IEnumerable<int> allowedChestTypes)
+        {
+            var allowedChestsHash = new HashSet<int>(allowedChestTypes);
+            var visitedChests = new HashSet<Chest>();
+            int itemsToPlace = Math.Min(count, 1000);
+            int tryLimit = 2000;
+
+            while (itemsToPlace > 0 && tryLimit-- > 0)
+            {
+                int chestIndex = Main.rand.Next(0, 999);
+                Chest chest = Main.chest[chestIndex];
+                if (chest != null && !visitedChests.Contains(chest))
+                {
+                    var chestTile = Main.tile[chest.x, chest.y];
+                    int chestType = chestTile.TileFrameX / 36;
+                    if (chestTile.TileType == TileID.Containers && allowedChestsHash.Contains(chestType))
+                    {
+                        int freeItemSlot = FindEmptySlotInChest(chest.item);
+                        if (freeItemSlot > 0)
+                        {
+                            chest.item[freeItemSlot].SetDefaults(itemTypeId);
+                            itemsToPlace--;
+                        }
+                    }
+                    visitedChests.Add(chest);
+                }
+            }
+        }
+
+        private static int FindEmptySlotInChest(Item[] chestSlots)
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                if (chestSlots[i].type == ItemID.None)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
